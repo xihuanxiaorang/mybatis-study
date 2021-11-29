4f30da3ce7d9cd28ebede20779da6b4e69326291
liulei <15019474951@163.com>
2021-11-29 15:11:50 +0800

    一对多查询（collection）

--- mybatis-common/src/main/java/top/xiaorang/mybatis/entity/Teacher.java
+++ mybatis-common/src/main/java/top/xiaorang/mybatis/entity/Teacher.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Teacher {
     private Integer id;
     private String name;
+    /**
+     * 一对多和多对一只选一种方式维护即可，这里是测试，所以才在teacher中维护students集合的同时，又在student中维护teacher
+     */
+    private List<Student> students;
 }
